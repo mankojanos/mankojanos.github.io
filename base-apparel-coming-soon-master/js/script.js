d93d88c96c220d2036a22eb32412f11c40798f04
Janos <mankojanos90@gmail.com>
2019-10-22 13:58:05 +0200

    js javítva
    háttér még megoldandó

--- base-apparel-coming-soon-master/js/script.js
+++ base-apparel-coming-soon-master/js/script.js
@@ -1,10 +1,29 @@
-function ValidateEmail(mail) 
-{
- if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(form1.email.value))
-  {
+function ValidateEmail(){
+ mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+ mail = document.getElementById("mail").value;
+ if (mail.match(mailFormat)){
     text = "&#10004";
 	document.getElementById("error").innerHTML = text;
-  }
+	}else{
     text = "Input not valid";
 	document.getElementById("error").innerHTML = text;
+	
+	}
 }
+function CheckEmail(){
+ mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+ mail = document.getElementById("mail").value;
+	if (mail.match(mailFormat)){
+		return true;
+	}else{
+		return false;
+	}
+}
+
+if(!CheckEmail()){
+	document.input.style.backgroundImage = "url(../images/icon-error.svg) no-repeat";
+}
+
+
+
+	
